7add168c3f8dd43c87f11611b5f3c972ca0c1ef1
erikdeirdre <pwhite@delpwhite.org>
2020-06-01 19:17:38 -0400

    add file

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+GITHUB_HEAD_REF=version-merge-4
+DIFF=$(git diff $GITHUB_HEAD_REF origin/master  -- VERSION)
+
+if [[ "$DIFF" ]]; then
+    IFS='.'; read -r -a array <<< $(cat VERSION)
+    CNT="${array[1]}"
+    ((CNT++))
+    echo "${array[0]}.${CNT}.${array[2]}"
+fi
+echo "here"
